b6ab38d9e343e7e78bd026579cf6367e9cd3265e
Cezary.Polosak <cezary.polosak@plus.pl>
2021-03-23 20:05:42 +0100

    init commit

---
+++ src/main/java/pl/czaki/jenkinsexampleczaki/JenkinsExampleCzakiApplication.java
@@ -0,0 +1,22 @@
+package pl.czaki.jenkinsexampleczaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsExampleCzakiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsExampleCzakiApplication.class, args);
+    }
+
+    @GetMapping
+    public String get(){
+        return "Yo ototot";
+    }
+
+
+}
